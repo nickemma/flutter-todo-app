97af43c3f6d6e0bd35da95798d108f73e777b219
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-11-17 13:45:37 +0100

    Initial commit for weather app

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(const MyApp());
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Weather App'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            'Hello World and welcome to my weather app',
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Text('Press'),
-        ));
+    return const MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    ));
   }
 }
